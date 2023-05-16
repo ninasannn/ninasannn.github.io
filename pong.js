d92606bacaa65a575c3a71197c75008b22a75f07
ninasannn <131017227+ninasannn@users.noreply.github.com>
2023-05-16 09:51:30 -0700

    Add files via upload

---
+++ pong.js
@@ -0,0 +1,41 @@
+var canvas = document.getElementById("gameCanvas");
+var canvasContext = canvas.getContext("2d");
+var ballX = 400;
+var ballY = 300;
+var ballSpeedX = 5;
+var ballSpeedY = 5;
+var ballRadius = 100;
+
+function drawBall() {
+    canvasContext.beginPath();
+    canvasContext.arc(ballX, ballY, ballRadius, 0, Math.PI*2, true);
+    canvasContext.fillStyle = "white";
+    canvasContext.fill();
+}
+
+function moveBall() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+  
+    // check for collision with left or right border
+    if (ballX - ballRadius < 0 || ballX + ballRadius > canvas.width) {
+      ballSpeedX = -ballSpeedX;
+    }
+  
+    // check for collision with top or bottom border
+    if (ballY - ballRadius < 0 || ballY + ballRadius > canvas.height) {
+      ballSpeedY = -ballSpeedY;
+    }
+}
+
+function draw() {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    canvasContext.fillStyle = "black";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+}
+
+setInterval(function() {
+    moveBall();
+    draw();
+}, 30);
